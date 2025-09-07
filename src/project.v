7f40c8e1adf10809611f26f00dd04d199ce8cccc
An Wei Teck <weiteck.an@gmail.com>
2025-09-07 16:26:11 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_2stageCMOSOpAmp (
+module tt_um_anweiteck_2stageCMOSOpAmp (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
